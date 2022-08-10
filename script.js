7fbfd002173cbf4302045e974e22fc7f2ef4051e
dnitra <109307365+dnitra@users.noreply.github.com>
2022-08-10 09:16:20 +0200

    layout, display content, styling

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log("hello")
+const presentCalculation = document.querySelector("#presentCalculation")
+
+
+const add = function(a,b) {
+	return parseInt(a)+parseInt(b)
+};
+
+const subtract = function(a,b) {
+	return parseInt(a)-parseInt(b)
+};
+
+
+const multiply = function(a,b) {
+  
+  return parseInt(a)*parseInt(b)
+};
+
+document.addEventListener("keydown",(e)=>{
+    const key = document.querySelector(`button[data-key="${e.keyCode}"`)
+    if(e.shiftKey)console.log(e.shiftKey)
+    if(!key)return
+    console.log(key.value)
+    console.log(e.keyCode)
+
+    if(presentCalculation.textContent.length<=21) presentCalculation.textContent+= key.value
+
+})
