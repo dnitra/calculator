6752116d23c918bb1fa3d063d2d2ee4721336ab9
dnitra <109307365+dnitra@users.noreply.github.com>
2022-08-11 13:26:32 +0200

    calculations work with keyboard input

--- script.js
+++ script.js
@@ -1,27 +1,103 @@
 const presentCalculation = document.querySelector("#presentCalculation")
+let firstNumber= ""
+let secondNumber= ""
+let pastNumber =""
+let op = ""
+let result = ""
+inicialize()
+
+const operators = {
+    
+    '=': ()=> {
+
+        firstNumber = pastNumber
+        secondNumber = presentCalculation.textContent
+        result = operators[op](firstNumber, secondNumber)
+        presentCalculation.textContent = result
+        op ="="
+        pastNumber=""
+        secondNumber=""
 
 
-const add = function(a,b) {
-	return parseInt(a)+parseInt(b)
+
+
+    },
+    'Backspace':()=>{
+        let str = presentCalculation.textContent.substring(0,presentCalculation.textContent.length-1)
+        presentCalculation.textContent = str
+        },
+    'Escape':inicialize,
+
+    '+': (a, b) => Number(a) + Number(b), 
+    '-': (a, b) => Number(a) - Number(b),
+    '*': (a, b) => Number(a) * Number(b), 
+    '/': (a, b) => Number(a) / Number(b),
+    
 };
 
-const subtract = function(a,b) {
-	return parseInt(a)-parseInt(b)
-};
 
 
-const multiply = function(a,b) {
-  
-  return parseInt(a)*parseInt(b)
-};
 
 document.addEventListener("keydown",(e)=>{
-    const key = document.querySelector(`button[data-key="${e.keyCode}"`)
-    if(e.shiftKey)console.log(e.shiftKey)
+    const key = document.querySelector(`button[value="${e.key}"`)
+   
     if(!key)return
-    console.log(key.value)
-    console.log(e.keyCode)
-
-    if(presentCalculation.textContent.length<=21) presentCalculation.textContent+= key.value
+    if(e.key in operators){
+        
+        if(e.key == "Escape" || e.key == "Backspace" || e.key == "="){
+            if (op == "=" && e.key!="Escape") return
+            else{
+             operators[e.key]()
+            }
+            }
+        else if(pastNumber.length==0){
+            pastNumber = presentCalculation.textContent
+            secondNumber = " "
+            op = e.key
+            return
+        }
+        
+        else if(pastNumber.length>0 ){
+            
+            firstNumber = pastNumber
+            op = e.key
+            secondNumber = presentCalculation.textContent
+            result = operators[op](firstNumber, secondNumber)
+            presentCalculation.textContent = result
+            pastNumber = result
+            op = e.key
+            
+            
+        }
+        
+    }
+    
+    else{
+        if (secondNumber == ""){
+            
+            presentCalculation.textContent+= key.value
+        }
+        else if(secondNumber!=""){
+            presentCalculation.textContent = ""
+            presentCalculation.textContent+= key.value
+            secondNumber=""
+        }
+            
+    }
 
 })
+
+function inicialize(){
+    firstNumber= ""
+    secondNumber= ""
+    op=""
+    presentCalculation.textContent=""
+}
+
+function calculate() {
+
+}
+
+function handleDisplay(){
+
+}
